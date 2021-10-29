c217f1e23b9e39142755da087d33b9ba37fe08c7
Antonio Alvarado <tonny.xp@hotmail.com>
2021-10-29 13:13:04 -0600

    first commit

---
+++ app/Controllers/Http/ProyectoController.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const Proyecto = use('App/Models/Proyecto')
+
+class ProyectoController {
+
+  async index ({ auth }) {
+    const user = await auth.getUser();   
+    return await user.proyectos().fetch(); 
+  }
+
+  async create ({ auth, request }) { 
+    const user = await auth.getUser();
+    const { nombre } = request.all();
+    
+    const proyecto = new Proyecto();
+    proyecto.fill({
+      nombre
+    });
+    await user.proyectos().save(proyecto);
+    return proyecto;
+  }
+
+  async destroy ({ auth, response, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const proyecto = await Proyecto.find(id);
+    if (proyecto.user_id !== user.id) {
+      return response.status(403).json({
+        message: 'No tienes permisos para eliminar este proyecto'
+      });
+    }
+    await proyecto.delete();
+    return proyecto;
+  }
+
+}
+
+module.exports = ProyectoController
---
+++ start/routes.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
+
+Route.group(() => {
+  Route.post('usuarios/login', 'UserController.login')
+  Route.post('usuarios/registro', 'UserController.store');
+  Route.get('proyectos', 'ProyectoController.index').middleware('auth');
+  Route.post('proyectos', 'ProyectoController.create').middleware('auth');
+  Route.delete('proyectos/:id', 'ProyectoController.destroy').middleware('auth');
+}).prefix('api');
